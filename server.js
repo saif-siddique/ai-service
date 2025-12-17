83dab9c6fd15d0bd77d4ee68a971adcf50ece027
Saif Siddique <saifsiddiqueshikrani@gmail.com>
2025-12-17 08:57:13 +0500

    change

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(cors({
   origin: "https://gravityhostel.vercel.app", // Ensure no trailing slash
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials : true
 }));
 
 app.use(express.json());
